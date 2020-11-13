a6db76ddd5f3090dc548878b0a1e989828f5ddff
Aris <aris.emery@gmail.com>
2020-11-12 22:09:18 -0700

    added comments

---
+++ build/CMakeFiles/ConwaysLife.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/arisemery/cs3460/conway/conway/LifeSimulator.cpp" "/Users/arisemery/cs3460/conway/conway/build/CMakeFiles/ConwaysLife.dir/LifeSimulator.cpp.o"
+  "/Users/arisemery/cs3460/conway/conway/PatternAcorn.cpp" "/Users/arisemery/cs3460/conway/conway/build/CMakeFiles/ConwaysLife.dir/PatternAcorn.cpp.o"
+  "/Users/arisemery/cs3460/conway/conway/PatternBlinker.cpp" "/Users/arisemery/cs3460/conway/conway/build/CMakeFiles/ConwaysLife.dir/PatternBlinker.cpp.o"
+  "/Users/arisemery/cs3460/conway/conway/PatternBlock.cpp" "/Users/arisemery/cs3460/conway/conway/build/CMakeFiles/ConwaysLife.dir/PatternBlock.cpp.o"
+  "/Users/arisemery/cs3460/conway/conway/PatternGlider.cpp" "/Users/arisemery/cs3460/conway/conway/build/CMakeFiles/ConwaysLife.dir/PatternGlider.cpp.o"
+  "/Users/arisemery/cs3460/conway/conway/PatternGosperGliderGun.cpp" "/Users/arisemery/cs3460/conway/conway/build/CMakeFiles/ConwaysLife.dir/PatternGosperGliderGun.cpp.o"
+  "/Users/arisemery/cs3460/conway/conway/RendererConsole.cpp" "/Users/arisemery/cs3460/conway/conway/build/CMakeFiles/ConwaysLife.dir/RendererConsole.cpp.o"
+  "/Users/arisemery/cs3460/conway/conway/main.cpp" "/Users/arisemery/cs3460/conway/conway/build/CMakeFiles/ConwaysLife.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
